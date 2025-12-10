03b09bb3cb3c32e2e1d7804661b9f3dbbc82c920
1rushabh1 <rushabhthaware1>
2025-12-10 16:16:23 +0530

    Initial commit - MERN project (frontend + backend)

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors({
